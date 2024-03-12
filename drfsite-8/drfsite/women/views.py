4ee0c8fbca00904f6fee107007a72876822af836
Умаров Камолиддин Салохиддин угли <k.umarov@agmk.uz>
2024-03-12 09:04:38 +0500

    rest-api

---
+++ drfsite-8/drfsite/women/views.py
@@ -0,0 +1,33 @@
+from django.forms import model_to_dict
+from rest_framework import generics, viewsets, mixins
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
+
+from .models import Women
+from .serializers import WomenSerializer
+
+
+class WomenViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+
+# class WomenAPIList(generics.ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
